0bbd0626278ff5fbe3aa1df5ef01a1cce2d38f50
don78 <35189266+don7667@users.noreply.github.com>
2018-01-22 03:42:34 +0500

    Update operation.go

--- src/github.com/openbankit/horizon/db2/history/operation.go
+++ src/github.com/openbankit/horizon/db2/history/operation.go
@@ -8,7 +8,7 @@ import (
 	"github.com/openbankit/horizon/toid"
 	"github.com/go-errors/errors"
 	"github.com/guregu/null"
-	sq "github.com/Masterminds/squirrel"
+	sq "github.com/lann/squirrel"
 	"time"
 )
 
