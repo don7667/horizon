cedb6bc290f7bb3c1a286652e55c416407f9a42e
don78 <35189266+don7667@users.noreply.github.com>
2018-01-22 01:58:20 +0500

    Update account_limits.go

--- src/github.com/openbankit/horizon/db2/history/account_limits.go
+++ src/github.com/openbankit/horizon/db2/history/account_limits.go
@@ -1,6 +1,6 @@
 package history
 
-import sq "github.com/lann/squirrel"
+import sq "github.com/Masterminds/squirrel"
 
 // GetAccountLimits returns limits row by account and asset.
 func (q *Q) GetAccountLimits(
